09535a2f956266b51d7d82b982fb2bede8e6075d
Feehl <100086502+Feehl@users.noreply.github.com>
2024-01-03 00:04:51 +0100

    #1 Update Programm
    changed program to include variable delay between measurements

--- RaketenSensorik/Assets/Scripts/CalculatePath.cs
+++ RaketenSensorik/Assets/Scripts/CalculatePath.cs
@@ -8,14 +8,16 @@ public class CalculatePath : MonoBehaviour
 {
     private string filepath;
     private List<Vector3> data = new List<Vector3>();
-    private float time;
+    private List<float> delays = new List<float>();
     public Transform rocketTransform;
     
     private Vector3 velocity = Vector3.zero;
     private float timer = 0;
     private int i = 0;
-    private float g = 9.81f;
+    private Vector3 g = new Vector3(0f, 9.81f, 0f);
     private bool running = true;
+    private string[] stringVectors;
+    private float currentDelay;
 
     // Start is called before the first frame update
     void Start()
@@ -29,35 +31,44 @@ public class CalculatePath : MonoBehaviour
 
     private void ReadFile(string path) 
     {
-        string[] stringVectors = File.ReadAllLines(path);
+        stringVectors = File.ReadAllLines(path);
 
-        time = float.Parse(stringVectors[0]) / 1000;
-
-        for(int i = 1; i < stringVectors.Length; i++)
+        for(int i = 0; i < stringVectors.Length; i+=3)
         {
-            string[] stringVector = stringVectors[i].Split(new string[] {";"}, StringSplitOptions.None);
-
-            if(stringVector.Length != 3) 
-            { 
-                print("Error"); 
-            }
-
-            data.Add(new Vector3(float.Parse(stringVector[0]), -float.Parse(stringVector[2]) + g, float.Parse(stringVector[1])));
+            delays.Add(float.Parse(stringVectors[i]));
+            AddStringVector(i+1);
+            AddStringVector(i+2);
         }
+
+        currentDelay = ConvertDelayToSeconds(delays[0]);
     }
 
+    private void AddStringVector(int index)
+    {
+        string[] stringVector = stringVectors[index].Split(new string[] { ";" }, StringSplitOptions.None);
+
+        if (stringVector.Length != 3)
+        {
+            print("Error");
+        }
+
+        data.Add(new Vector3(float.Parse(stringVector[0]), float.Parse(stringVector[2]), float.Parse(stringVector[1])));
+    }
+
+    //Simulate
     private void Update()
     {
         if (i >= data.Count / 2) return;
         if(!running) return;
         timer += Time.deltaTime;
-        if(timer > time)
+        if(timer >= currentDelay)
         {
             running = false;
             CalculateVelocity(i);
             CalculatePosition();
             CalculateRotation(i + 1);
             i += 2;
+            currentDelay = ConvertDelayToSeconds(delays[i / 2] - delays[(i / 2) - 1]);
             timer = 0;
             running = true;
         }
@@ -65,16 +76,21 @@ public class CalculatePath : MonoBehaviour
 
     private void CalculateRotation(int index)
     {
-        Vector3 rotationAngle = data[index] * Mathf.Rad2Deg * time;
+        Vector3 rotationAngle = data[index] * Mathf.Rad2Deg * currentDelay;
         rocketTransform.Rotate(rotationAngle, Space.Self);
     }
 
     private void CalculateVelocity(int index)
     {
-        velocity += (data[index] * time);
+        velocity += ((data[index]+g) * currentDelay);
     }
     private void CalculatePosition()
     {
-        rocketTransform.Translate(velocity * time);
+        rocketTransform.Translate(velocity * currentDelay);
+    }
+
+    private float ConvertDelayToSeconds(float delayInMs)
+    {
+        return delayInMs / 1000f;
     }
 }
