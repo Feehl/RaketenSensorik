1abe4eac0f5cbaeef0a0e26ac38f20a0107698ba
Feehl <100086502+Feehl@users.noreply.github.com>
2024-01-03 15:13:16 +0100

    Berechnung gefixt
    Acceleration now depending on rotation of the rocket (replaced velocity vector with gameobject)
    fixed broken cycle only going through half of the data

--- RaketenSensorik/Assets/Scripts/CalculatePath.cs
+++ RaketenSensorik/Assets/Scripts/CalculatePath.cs
@@ -10,8 +10,10 @@ public class CalculatePath : MonoBehaviour
     private List<Vector3> data = new List<Vector3>();
     private List<float> delays = new List<float>();
     public Transform rocketTransform;
-    
-    private Vector3 velocity = Vector3.zero;
+    public Transform velocityObject;
+    public bool applyGravity;
+    public bool lockHeight;
+
     private float timer = 0;
     private int i = 0;
     private Vector3 g = new Vector3(0f, 9.81f, 0f);
@@ -33,15 +35,14 @@ public class CalculatePath : MonoBehaviour
     private void ReadFile(string path) 
     {
         stringVectors = File.ReadAllLines(path);
+        delays.Add(float.Parse(stringVectors[0]));
 
-        for(int i = 0; i < stringVectors.Length; i+=3)
+        for(int i = 1; i < stringVectors.Length; i+=3)
         {
-            delays.Add(float.Parse(stringVectors[i]));
+            AddStringVector(i);
             AddStringVector(i+1);
-            AddStringVector(i+2);
+            delays.Add(float.Parse(stringVectors[i+2]));
         }
-
-        currentDelay = ConvertDelayToSeconds(delays[0]);
     }
 
     private void AddStringVector(int index)
@@ -59,40 +60,60 @@ public class CalculatePath : MonoBehaviour
     //Simulate
     private void Update()
     {
+        //End
         if(finished) return;
-        if (i >= data.Count / 2)
+        if (i >= data.Count)
         {
             finished = true;
             Debug.Log("Final Height: " + rocketTransform.position.y.ToString());
+            return;
         }
-        if(!running) return;
+
+        //Timer
+        if (!running) return;
         timer += Time.deltaTime;
         if(timer >= currentDelay)
         {
             running = false;
-            CalculateVelocity(i+1);
+            currentDelay = ConvertDelayToSeconds(delays[(i / 2)+1] - delays[(i / 2)]);
+
+            CalculateVelocity(i);
             CalculatePosition();
-            CalculateRotation(i);
+            CalculateRotation(i+1);
+            
             i += 2;
-            currentDelay = ConvertDelayToSeconds(delays[i / 2] - delays[(i / 2) - 1]);
             timer = 0;
+            Debug.Log(currentDelay.ToString() + " seconds passed");
             running = true;
         }
     }    
 
     private void CalculateRotation(int index)
     {
-        Vector3 rotationAngle = data[index] * Mathf.Rad2Deg * currentDelay;
+        Vector3 rotationAngle = data[index] * currentDelay * Mathf.Rad2Deg;
         rocketTransform.Rotate(rotationAngle, Space.Self);
+        velocityObject.Rotate(rotationAngle, Space.Self);
     }
 
     private void CalculateVelocity(int index)
     {
-        velocity += ((data[index]+g) * currentDelay);
+        if(applyGravity) 
+            ApplyGravity();
+
+        velocityObject.Translate(data[index] * currentDelay, Space.Self);
+
+        if(lockHeight)
+            velocityObject.position = new Vector3(velocityObject.position.x, 0f, velocityObject.position.z);
     }
+
     private void CalculatePosition()
     {
-        rocketTransform.Translate(velocity * currentDelay);
+        rocketTransform.Translate(velocityObject.localPosition * currentDelay, Space.World);
+    }
+
+    private void ApplyGravity()
+    {
+        velocityObject.Translate(g * currentDelay, Space.World);
     }
 
     private float ConvertDelayToSeconds(float delayInMs)
